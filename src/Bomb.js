fc718b18044a76f16794be6ba06fb1025db407d4
Olusola Olagoroye <solaolagoroye@gmail.com>
2020-04-27 10:21:34 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,19 @@
 // your Bomb code here!
+
+import React, { Component } from 'react';
+
+class Bomb extends Component{
+    constructor(){
+        super();
+        this.state = {
+            currentSlideIndex: 0 
+        }
+
+    }
+    reder() {
+        return(
+        <div></div>)
+    }
+
+}
+export default Bomb;
